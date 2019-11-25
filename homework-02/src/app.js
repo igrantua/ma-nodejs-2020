1f0330c08d67687079a93253f0186585b29cd80c
igrantua <igrantua@gmail.com>
2019-11-25 21:52:04 +0200

    fix await+then

--- homework-02/src/app.js
+++ homework-02/src/app.js
@@ -1,10 +1,14 @@
-const { sum: addThreeNumbers } = require('./task');
-const { planet: earth } = require('./task');
-const { later } = require('./task');
+const { sum: addThreeNumbers, planet: earth, later } = require('./task');
 
 const boot = async () => {
   console.log(addThreeNumbers(1, 2, 4));
-  await later.then(console.log);
+  // await later.then(console.log);
+  try {
+    const result = await later;
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
   console.log(earth.info());
 };
 
