72dbca02f81c5971f2827f11127af0d924ca0a81
igrantua <igrantua@gmail.com>
2019-12-14 11:15:27 +0200

    homework-05

---
+++ homework-05/src/index.js
@@ -0,0 +1,102 @@
+/* eslint-disable no-restricted-syntax */
+const path = require('path');
+const fsp = require('fs').promises;
+const zlib = require('zlib');
+const { URL } = require('url');
+const { promisify } = require('util');
+
+const gunzip = promisify(zlib.gunzip);
+const gzip = promisify(zlib.gzip);
+
+const inputDirName = 'input';
+const outputDirName = 'output';
+const outputFileName = 'result.json.gz';
+
+const inputDir = path.join(process.cwd(), inputDirName);
+const outputFile = path.join(process.cwd(), outputDirName, outputFileName);
+
+async function getInputFileList() {
+  let files;
+  try {
+    files = await fsp.readdir(inputDirName);
+  } catch (error) {
+    console.error(`Error while reading ${inputDirName}`);
+  }
+  return files.map((file) => path.join(inputDir, file));
+}
+
+async function getObjectFromFile(filePath) {
+  let compressedBuffer;
+  try {
+    compressedBuffer = await fsp.readFile(filePath);
+  } catch (error) {
+    console.error(`Error while reading ${filePath}`);
+  }
+  let jsonBuffer;
+  try {
+    jsonBuffer = await gunzip(compressedBuffer);
+  } catch (error) {
+    console.error(`Error while extracting ${compressedBuffer}`);
+  }
+  let object;
+  let json;
+  try {
+    json = jsonBuffer.toString();
+    object = JSON.parse(json);
+  } catch (error) {
+    console.error(`Error while parsing ${json}`);
+  }
+  return object;
+}
+
+function rebuildUrl(originalUrl) {
+  const myURL = new URL(originalUrl);
+  const { pathname } = myURL;
+  const fileType = path.extname(pathname);
+  const deviceType = path.basename(pathname, fileType);
+  myURL.protocol = 'https';
+  myURL.pathname = '/devices';
+  myURL.search = `?file=${deviceType}&type=${fileType}`;
+  return myURL;
+}
+
+async function buildOutputObject(files) {
+  const result = {};
+  for (const file of files) {
+    let object;
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      object = await getObjectFromFile(file);
+    } catch (error) {
+      console.error(`Error while getting object from ${file}`);
+    }
+    object.url = rebuildUrl(object.url);
+    const name = path.basename(file.toLowerCase(), '.json.gz');
+    result[name] = object;
+  }
+  return result;
+}
+
+async function saveOutput(object) {
+  const stringifiedObject = JSON.stringify(object);
+  const buffer = Buffer.from(stringifiedObject);
+  let compressedBuffer;
+  try {
+    compressedBuffer = await gzip(buffer);
+  } catch (error) {
+    console.error(`Error while compressing ${buffer}`);
+  }
+  try {
+    await fsp.writeFile(outputFile, compressedBuffer);
+  } catch (error) {
+    console.error(`Error while writing to  ${outputFile}`);
+  }
+}
+
+async function start() {
+  const inputFiles = await getInputFileList();
+  const outputObject = await buildOutputObject(inputFiles);
+  await saveOutput(outputObject);
+}
+
+start().catch(err => console.error('🐞  🤪  🐛\n', err));
