7437e76c792a4e69080a2896a0ab3b9ec40d121e
igrantua <igrantua@gmail.com>
2019-12-22 19:54:35 +0200

    fix COLOR work

--- homework-06/app.js
+++ homework-06/app.js
@@ -1,11 +1,11 @@
 // #!/usr/bin/env node
 const os = require('os');
 
-let RATE = process.env.RATE || 1000;
-let LIMIT = process.env.LIMIT || 300;
-let COLOR = process.env.COLOR || true;
+const RATE = process.env.RATE || 1000;
+const LIMIT = process.env.LIMIT || 300;
+const COLOR = process.env.COLOR !== 'false';
 
-let PreviouslyUsedMemory = 0;
+let previouslyUsedMemory = 0;
 const msg = `!!! ATTENTION: Available memory is under the defined limit !!!`;
 
 const memoryLog = () => {
@@ -13,10 +13,9 @@ const memoryLog = () => {
   const totalMemory = os.totalmem() / 1024 / 1024;
   const freeMemory = os.freemem() / 1024 / 1024;
   const usedMemory = totalMemory - freeMemory;
-  const deltaMemory = usedMemory - PreviouslyUsedMemory;
-  PreviouslyUsedMemory = usedMemory;
+  const deltaMemory = usedMemory - previouslyUsedMemory;
+  previouslyUsedMemory = usedMemory;
 
-  // eslint-disable-next-line no-cond-assign
   if (COLOR) {
     console.log(`Total system memory: ${totalMemory.toFixed()} MB`);
 
@@ -24,14 +23,18 @@ const memoryLog = () => {
     if (freeMemory < LIMIT) {
       colorFreeMemory = `\x1b[31m${freeMemory.toFixed(3)}\x1b[0m`;
       console.log(`Free memory available: ${colorFreeMemory} MB`);
-    } else console.log(`Free memory available: ${freeMemory.toFixed(3)} MB`);
+    } else {
+      console.log(`Free memory available: ${freeMemory.toFixed(3)} MB`);
+    }
 
     console.log(`Allocated memory: ${usedMemory.toFixed(3)} MB`);
 
     let colorDelta;
     if (deltaMemory <= 0) {
       colorDelta = `\x1b[31m${deltaMemory.toFixed(3)}\x1b[0m`;
-    } else colorDelta = `\x1b[32m${deltaMemory.toFixed(3)}\x1b[0m`;
+    } else {
+      colorDelta = `\x1b[32m${deltaMemory.toFixed(3)}\x1b[0m`;
+    }
     console.log(`Delta for previous allocated memory value: ${colorDelta} MB`);
     if (freeMemory < LIMIT) {
       console.log(`\x1b[31m${msg}\x1b[0m`);
@@ -41,6 +44,9 @@ const memoryLog = () => {
     console.log(`Free memory available: ${freeMemory.toFixed(3)} MB`);
     console.log(`Allocated memory: ${usedMemory.toFixed(3)} MB`);
     console.log(`Delta for previous allocated memory value: ${deltaMemory.toFixed(3)} MB`);
+    if (freeMemory < LIMIT) {
+      console.log(`${msg}`);
+    }
   }
 };
 
